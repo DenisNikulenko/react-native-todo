92b15843b4724c4b9356013bc5bb871e71306fe1
Denis <delttol@gmail.com>
2021-01-24 19:19:06 +0200

    added reducer, types and ...

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -9,55 +9,55 @@ import { THEME } from "./theme";
 import { TodoContext } from "./context/todo/todoContext";
 
 export const MainLayout = () => {
-  const todoContext = useContext(TodoContext)
+  const  {todos, addTodo, removeTodo, updateTodo} = useContext(TodoContext)
 	const [todoId, setTodoId] = useState(null);
-	const [todos, setTodos] = useState([]);
+	// const [todos, setTodos] = useState([]);
 	
-	const addTodo = title => {
-    const newTodo = {
-      id: Date.now().toString(),
-      title,
-    };
-    setTodos(prev => [...prev, newTodo]);
-  };
+	// const addTodo = title => {
+  //   const newTodo = {
+  //     id: Date.now().toString(),
+  //     title,
+  //   };
+  //   setTodos(prev => [...prev, newTodo]);
+  // };
 
-  const removeTodo = id => {
-    const todo = todos.find((t) => t.id === id);
+  // const removeTodo = id => {
+  //   const todo = todos.find((t) => t.id === id);
 
-    Alert.alert(
-      "Удаление элемена",
-      `Вы уверены, что хотите удалить "${todo.title}"?`,
-      [
-        {
-          text: "Отмена",
-          style: "cancel",
-        },
-        {
-          text: "Удалить",
-          onPress: () => {
-            setTodoId(null);
-            setTodos((prev) => prev.filter((todo) => todo.id !== id));
-          },
-        },
-      ],
-      { cancelable: false }
-    );
-  };
+  //   Alert.alert(
+  //     "Удаление элемена",
+  //     `Вы уверены, что хотите удалить "${todo.title}"?`,
+  //     [
+  //       {
+  //         text: "Отмена",
+  //         style: "cancel",
+  //       },
+  //       {
+  //         text: "Удалить",
+  //         onPress: () => {
+  //           setTodoId(null);
+  //           setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  //         },
+  //       },
+  //     ],
+  //     { cancelable: false }
+  //   );
+  // };
 
-  const updateTodo = (id, title) => {
-    setTodos(old =>
-      old.map(todo => {
-        if (todo.id === id) {
-          todo.title = title;
-        }
-        return todo;
-      })
-    );
-	};
+  // const updateTodo = (id, title) => {
+  //   setTodos(old =>
+  //     old.map(todo => {
+  //       if (todo.id === id) {
+  //         todo.title = title;
+  //       }
+  //       return todo;
+  //     })
+  //   );
+	// };
 	
 	let content = (
     <MainScreen
-      todos={todoContext.todos}
+      todos={todos}
       addTodo={addTodo}
       removeTodo={removeTodo}
       openTodo={id => {
--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -1,23 +1,40 @@
-import React, { useReducer } from 'react'
-import { TodoContext } from './todoContext'
-import { todoReducer } from './todoReducer'
+import React, { useReducer } from "react";
+import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from "../types";
+import { TodoContext } from "./todoContext";
+import { todoReducer } from "./todoReducer";
 
 export const TodoState = ({ children }) => {
   const initialState = {
     todos: [
       { id: "1", title: "Выучить React Native" },
-      { id: "2", title: "test context"}
-    ]
-  }
-  const [state, dispatch] = useReducer(todoReducer, initialState)
+      { id: "2", title: "test context" },
+    ],
+  };
+
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+
+  const addTodo = (title) => {
+    dispatch({ type: ADD_TODO, title });
+  };
+
+  const removeTodo = (id) => {
+    dispatch({ type: REMOVE_TODO, id });
+  };
+
+  const updateTodo = (id, title) => {
+    dispatch({ type: UPDATE_TODO, id, title });
+  };
 
   return (
     <TodoContext.Provider
       value={{
-        todos: state.todos
+        todos: state.todos,
+        addTodo,
+        removeTodo,
+        updateTodo,
       }}
     >
       {children}
     </TodoContext.Provider>
-  )
-}
+  );
+};
